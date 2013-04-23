b90c0beacb1812c1e852fcdf3189532b14ffad7b
Marlin Cremers <m.cremers@mms-projects.net>
2013-04-24 00:44:58 +0200

    Split the main code, desktop app and Android app into their own projects

---
+++ android/src/net/mms_projects/copyit/android/tasks/CopyItTask.java
@@ -0,0 +1,49 @@
+package net.mms_projects.copyit.android.tasks;
+
+import net.mms_projects.copy_it.R;
+import net.mms_projects.copyit.api.ServerApi;
+import net.mms_projects.copyit.api.endpoints.ClipboardContentEndpoint;
+import android.content.Context;
+import android.widget.Toast;
+
+public class CopyItTask extends ServerApiUiTask<String, Void, Boolean> {
+	public CopyItTask(Context context, ServerApi api) {
+		super(context, api);
+	}
+
+	private String content;
+
+	@Override
+	protected Boolean doInBackgroundWithException(String... params)
+			throws Exception {
+		this.content = params[0];
+
+		return new ClipboardContentEndpoint(api).update(this.content);
+	}
+
+	@Override
+	protected void onPostExecute(Boolean result) {
+		try {
+			this.doExceptionCheck();
+
+			if (result) {
+				Toast.makeText(
+						this.context,
+						this.context.getResources().getString(
+								R.string.text_content_pushed, this.content),
+						Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+			Toast.makeText(
+					this.context,
+					this.context.getResources().getString(
+							R.string.error_general, e.getLocalizedMessage()),
+					Toast.LENGTH_LONG).show();
+		}
+
+		super.onPostExecute(result);
+	}
+}
---
+++ android/src/net/mms_projects/copyit/android/tasks/PasteItTask.java
@@ -0,0 +1,48 @@
+package net.mms_projects.copyit.android.tasks;
+
+import net.mms_projects.copy_it.R;
+import net.mms_projects.copyit.AndroidClipboardUtils;
+import net.mms_projects.copyit.ClipboardUtils;
+import net.mms_projects.copyit.api.ServerApi;
+import net.mms_projects.copyit.api.endpoints.ClipboardContentEndpoint;
+import android.content.Context;
+import android.widget.Toast;
+
+public class PasteItTask extends ServerApiUiTask<Void, Void, String> {
+	public PasteItTask(Context context, ServerApi api) {
+		super(context, api);
+	}
+
+	@Override
+	protected String doInBackgroundWithException(Void... params)
+			throws Exception {
+		return new ClipboardContentEndpoint(api).get();
+	}
+
+	@Override
+	protected void onPostExecute(String content) {
+		try {
+			this.doExceptionCheck();
+
+			ClipboardUtils clipboard = new AndroidClipboardUtils(this.context);
+
+			Toast.makeText(
+					this.context,
+					this.context.getResources().getString(
+							R.string.text_content_pulled, content),
+					Toast.LENGTH_LONG).show();
+			clipboard.setText(content);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+			Toast.makeText(
+					this.context,
+					this.context.getResources().getString(
+							R.string.error_general, e.getLocalizedMessage()),
+					Toast.LENGTH_LONG).show();
+		}
+
+		super.onPostExecute(content);
+	}
+}
