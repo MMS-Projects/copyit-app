83bf248e65a00e37ff4ba7102c81040bfa70f200
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-04-24 22:34:22 +0200

    Fix the infinite UI loop in the WelcomeActivity #48
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- android/src/net/mms_projects/copyit/ui/android/WelcomeActivity.java
+++ android/src/net/mms_projects/copyit/ui/android/WelcomeActivity.java
@@ -12,46 +12,15 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 public class WelcomeActivity extends SherlockActivity {
-
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
-		// Show the Up button in the action bar.
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 	}
 
 	public void doLogin(View view) {
 		Intent intent = new Intent(this, LoginActivity.class);
-		this.startActivity(intent);
-		this.finish();
+		startActivity(intent);
+		finish();
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
-
 }
