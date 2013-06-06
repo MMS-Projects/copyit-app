2746e8ca773192b7b8f650b42a1197c2715d1163
Marlin Cremers <m.cremers@mms-projects.net>
2013-06-06 21:34:21 +0200

    Implemented a new design with ads in the bottom
    I've implemented a new design removes the settings
    and about dashboard buttons. This provides more room
    for on the dashboard. This new room allows us to have
    ads which I've added too.

--- android/test/net/mms_projects/copy_it/tests/MainActivityTest.java
+++ android/test/net/mms_projects/copy_it/tests/MainActivityTest.java
@@ -2,7 +2,6 @@ package net.mms_projects.copy_it.tests;
 
 import net.mms_projects.copyit.ui.android.MainActivity;
 import android.content.Intent;
-import android.widget.Button;
 
 public class MainActivityTest extends
 		android.test.ActivityUnitTestCase<MainActivity> {
@@ -21,17 +20,6 @@ public class MainActivityTest extends
 				MainActivity.class);
 		startActivity(intent, null, null);
 		activity = getActivity();
-	}
-
-	public void testLayout() {
-		int buttonSettings = net.mms_projects.copy_it.R.id.button_settings;
-		assertNotNull(activity.findViewById(buttonSettings));
-		Button buttonViewSettings = (Button) activity
-				.findViewById(buttonSettings);
-		assertEquals(
-				"Incorrect label of the button",
-				activity.getString(net.mms_projects.copy_it.R.string.dashboard_button_settings),
-				buttonViewSettings.getText());
 	}
 
 }
