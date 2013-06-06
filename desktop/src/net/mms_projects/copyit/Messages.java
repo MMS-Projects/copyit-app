4c2f1d77edae0001c3390ebed7d329751d1554e7
Marlin Cremers <m.cremers@mms-projects.net>
2013-06-06 22:02:15 +0200

    Added support for translation on the desktop client which fixes issue #97
    I've added translation support to the messages
    class. And I've added a example file for the
    Dutch language.

--- desktop/src/net/mms_projects/copyit/Messages.java
+++ desktop/src/net/mms_projects/copyit/Messages.java
@@ -1,21 +1,26 @@
 package net.mms_projects.copyit;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
 	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
+	private static ResourceBundle RESOURCE_BUNDLE;
+	private static ResourceBundle FALLBACK_RESOURCE_BUNDLE;
 
 	private Messages() {
 	}
 
 	public static String getString(String key) {
 		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
+			return getBundle().getString(key);
+		} catch (MissingResourceException e1) {
+			try {
+				return getFallbackBundle().getString(key);
+			} catch (MissingResourceException e2) {
+			}
 		}
 		String text = AndroidResourceLoader.getString("@string/" + key);
 		if (text != null) {
@@ -27,5 +32,20 @@ public class Messages {
 	public static String getString(String key, Object... formatArgs) {
 		String raw = getString(key);
 		return String.format(raw, formatArgs);
+	}
+
+	private static ResourceBundle getBundle() {
+		Locale locale = Locale.getDefault();
+		if (RESOURCE_BUNDLE == null) {
+			RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+		}
+		return RESOURCE_BUNDLE;
+	}
+
+	private static ResourceBundle getFallbackBundle() {
+		if (FALLBACK_RESOURCE_BUNDLE == null) {
+			FALLBACK_RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+		}
+		return FALLBACK_RESOURCE_BUNDLE;
 	}
 }
