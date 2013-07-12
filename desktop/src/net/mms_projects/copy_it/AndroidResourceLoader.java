5f0fa52ab2ec5641575654568221dc84007900f0
Marlin Cremers <m.cremers@mms-projects.net>
2013-07-12 13:34:03 +0200

    Marked the AndroidResourceLoader as deprecated

--- desktop/src/net/mms_projects/copy_it/AndroidResourceLoader.java
+++ desktop/src/net/mms_projects/copy_it/AndroidResourceLoader.java
@@ -15,6 +15,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+@Deprecated
 public class AndroidResourceLoader {
 
 	private static Map<String, String> strings;
