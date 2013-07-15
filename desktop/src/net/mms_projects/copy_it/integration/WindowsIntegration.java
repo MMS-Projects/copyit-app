ae941fa887b17a7ba1ce6b714d51cdb6b5f5fa93
Marlin Cremers <m.cremers@mms-projects.net>
2013-07-15 09:29:11 +0200

    Added a Windows specific integration

---
+++ desktop/src/net/mms_projects/copy_it/integration/WindowsIntegration.java
@@ -0,0 +1,37 @@
+package net.mms_projects.copy_it.integration;
+
+import net.mms_projects.copy_it.ClipboardManager;
+import net.mms_projects.copy_it.EnvironmentIntegration;
+import net.mms_projects.copy_it.Settings;
+import net.mms_projects.copy_it.SyncManager;
+
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * This integration provider adds Windows specific integrations.
+ */
+public class WindowsIntegration extends EnvironmentIntegration {
+
+	public WindowsIntegration(Settings settings, Shell activityShell,
+			SyncManager syncManager, ClipboardManager clipboardManager) {
+		/*
+		 * Adds SWT integration like a tray icon
+		 */
+		BasicSwtIntegration swtIntegration = new BasicSwtIntegration(this,
+				settings, activityShell, syncManager, clipboardManager);
+		this.addIntegration(swtIntegration);
+
+		/*
+		 * Add some listeners to the SWT integration
+		 */
+		syncManager.addListener(swtIntegration);
+		clipboardManager.addListener(swtIntegration);
+	}
+
+	@Override
+	public void standaloneSetup() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
