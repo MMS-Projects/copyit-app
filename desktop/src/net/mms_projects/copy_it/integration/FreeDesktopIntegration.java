de46bac69d0992519bbb3659f5cea8426ddc4f24
Marlin Cremers <m.cremers@mms-projects.net>
2013-07-15 10:40:39 +0200

    Seperated the Java command line generator from the FreeDesktop integration
    Because we will need this for the Windows auto start functionality
    this needs to be split off.

--- desktop/src/net/mms_projects/copy_it/integration/FreeDesktopIntegration.java
+++ desktop/src/net/mms_projects/copy_it/integration/FreeDesktopIntegration.java
@@ -2,16 +2,14 @@ package net.mms_projects.copy_it.integration;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
 import net.mms_projects.copy_it.EnvironmentIntegration;
+import net.mms_projects.copy_it.JavaCommandLine;
 import net.mms_projects.copy_it.Messages;
 import net.mms_projects.copy_it.PathBuilder;
-import net.mms_projects.copy_it.app.CopyItDesktop;
 import net.mms_projects.copy_it.integration.notifications.FreedesktopNotificationManager;
 
 import org.apache.commons.io.FileUtils;
@@ -115,64 +113,10 @@ public class FreeDesktopIntegration extends EnvironmentIntegration {
 		content.add("Type=Application");
 		content.add("Name=" + Messages.getString("app_name"));
 		content.add("Icon=copyit");
-		content.add("Exec=" + this.generateJavaCommandLine() + " %f");
+		content.add("Exec=" + JavaCommandLine.generateJavaCommandLine() + " %f");
 		content.add("Terminal=false");
 		content.add("StartupNotify=true");
 		return content;
-	}
-
-	/**
-	 * This method generates a java command line that can be used to run the
-	 * application the way it was started in this runtime
-	 * 
-	 * @return A string with the Java command line
-	 */
-	private String generateJavaCommandLine() {
-		List<String> commandline = new ArrayList<String>();
-
-		commandline.add("java");
-		/*
-		 * This adds some the command line arguments passed to the current JVM
-		 */
-		commandline.addAll(ManagementFactory.getRuntimeMXBean()
-				.getInputArguments());
-		/*
-		 * This adds the current class path
-		 */
-		commandline.add("-classpath \"" + getClasspath() + "\"");
-		/*
-		 * This adds the main class to the command line
-		 */
-		commandline.add(CopyItDesktop.class.getName());
-
-		/*
-		 * Converts the array of command line arguments to a long string with
-		 * all the command line arguments. Delimited by a string.
-		 */
-		String rawCommandline = "";
-		for (String part : commandline) {
-			rawCommandline += part + " ";
-		}
-
-		return rawCommandline;
-	}
-
-	/**
-	 * This methods returns the class path used by the current runtime
-	 * 
-	 * @return Returns the class path used by the current runtime
-	 */
-	private String getClasspath() {
-		String classpath = "";
-		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-
-		URL[] urls = ((URLClassLoader) classLoader).getURLs();
-
-		for (URL url : urls) {
-			classpath += url.getFile();
-			classpath += System.getProperty("path.separator");
-		}
-		return classpath;
 	}
 
 	/**
