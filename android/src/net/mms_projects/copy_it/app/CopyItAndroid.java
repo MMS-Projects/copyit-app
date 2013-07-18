bdd0de1d165a52bdb9bce63d8f1e61abd277b026
Marlin Cremers <m.cremers@mms-projects.net>
2013-07-18 20:08:10 +0200

    Removed the old AbstractUi class

--- android/src/net/mms_projects/copy_it/app/CopyItAndroid.java
+++ android/src/net/mms_projects/copy_it/app/CopyItAndroid.java
@@ -1,11 +1,7 @@
 package net.mms_projects.copy_it.app;
 
-import net.mms_projects.copy_it.R;
 import net.mms_projects.copy_it.Config;
-import net.mms_projects.copy_it.app.CopyIt;
-import net.mms_projects.copy_it.ui.AndroidGui;
 import net.mms_projects.copy_it.ui.android.MainActivity;
-import net.mms_projects.copy_it.ui_old.AbstractUi;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 
@@ -16,8 +12,6 @@ public class CopyItAndroid extends CopyIt {
 	public void run(MainActivity activity) {
 		this.settings = new Config();
 
-		AbstractUi ui = new AndroidGui(this.settings, activity);
-		ui.open();
 	}
 	
 	public static int getBuildNumber(Context context) {
--- global/src/net/mms_projects/copy_it/ui/SingleCommandUi.java
+++ global/src/net/mms_projects/copy_it/ui/SingleCommandUi.java
@@ -1,45 +1,45 @@
 package net.mms_projects.copy_it.ui;
 
+import java.io.PrintStream;
+
 import net.mms_projects.copy_it.ClipboardManager;
 import net.mms_projects.copy_it.Config;
 import net.mms_projects.copy_it.SyncManager;
-import net.mms_projects.copy_it.ui.UserInterfaceImplementation.AboutUserInterface;
-import net.mms_projects.copy_it.ui.UserInterfaceImplementation.SettingsUserInterface;
 import net.mms_projects.copy_it.ui.interactive_shell.commands.CopyIt;
 import net.mms_projects.copy_it.ui.interactive_shell.commands.PasteIt;
-import net.mms_projects.copy_it.ui_old.AbstractUi;
 import net.mms_projects.irc.channel_bots.pb.CommandHandler;
 import net.mms_projects.irc.channel_bots.pb.commands.Help;
-
-import java.io.PrintStream;
 
 public class SingleCommandUi implements UserInterfaceImplementation {
 
 	private SettingsUserInterface settingsUserInterface;
 	private AboutUserInterface aboutUserInterface;
-	
-    private SyncManager syncManager;
-    private ClipboardManager clipboardManager;
-    private String command;
 
-    public SingleCommandUi(Config settings, SyncManager syncManager, ClipboardManager clipboardManager, String command) {
+	private SyncManager syncManager;
+	private ClipboardManager clipboardManager;
+	private String command;
 
-        this.syncManager = syncManager;
-        this.clipboardManager = clipboardManager;
-        this.command = command;
-    }
+	public SingleCommandUi(Config settings, SyncManager syncManager,
+			ClipboardManager clipboardManager, String command) {
 
-    @Override
-    public void open() {
-        PrintStream printStream = new PrintStream(System.out);
+		this.syncManager = syncManager;
+		this.clipboardManager = clipboardManager;
+		this.command = command;
+	}
 
-        CommandHandler commandHandler = new CommandHandler();
-        commandHandler.setPrintStream(printStream);
-        commandHandler.addCommand(new Help(commandHandler));
-        commandHandler.addCommand(new CopyIt(commandHandler, this.syncManager, this.clipboardManager));
-        commandHandler.addCommand(new PasteIt(commandHandler, this.syncManager, this.clipboardManager));
-        commandHandler.handle(this.command);
-    }
+	@Override
+	public void open() {
+		PrintStream printStream = new PrintStream(System.out);
+
+		CommandHandler commandHandler = new CommandHandler();
+		commandHandler.setPrintStream(printStream);
+		commandHandler.addCommand(new Help(commandHandler));
+		commandHandler.addCommand(new CopyIt(commandHandler, this.syncManager,
+				this.clipboardManager));
+		commandHandler.addCommand(new PasteIt(commandHandler, this.syncManager,
+				this.clipboardManager));
+		commandHandler.handle(this.command);
+	}
 
 	@Override
 	public void close() {
