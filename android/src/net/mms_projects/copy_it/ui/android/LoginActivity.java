18adf32ceca27e9618bc2faa1cfab7de1e04305c
Marlin Cremers <m.cremers@mms-projects.net>
2013-07-27 14:28:32 +0200

    Added a Facebook login button to the login activity

--- android/src/net/mms_projects/copy_it/ui/android/LoginActivity.java
+++ android/src/net/mms_projects/copy_it/ui/android/LoginActivity.java
@@ -7,9 +7,15 @@ import net.mms_projects.copy_it.R;
 import net.mms_projects.copy_it.activities.BrowserLoginActivity;
 import net.mms_projects.copy_it.android.tasks.SetupDeviceTask;
 import net.mms_projects.copy_it.api.ServerApi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -21,35 +27,132 @@ import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class LoginActivity extends SherlockActivity {
 
 	private static final int ACTIVITY_LOGIN = 1;
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
+	private UiLifecycleHelper uiHelper;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		this.uiHelper = new UiLifecycleHelper(this, callback);
+		this.uiHelper.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_login);
 		// Show the Up button in the action bar.
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-		Intent intent = new Intent(this, BrowserLoginActivity.class);
-		startActivityForResult(intent, LoginActivity.ACTIVITY_LOGIN);
 	}
-	
+
+	private void onSessionStateChange(final Session session,
+			final SessionState state, final Exception exception) {
+		if (state.isOpened()) {
+			this.log.info("Logged in...");
+
+			Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(this.getString(
+					R.string.login_dialog_text_automatic_service_login,
+					"Facebook"));
+			builder.setCancelable(true);
+			builder.setPositiveButton(R.string.dialog_button_yes,
+					new OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							/*
+							 * Close the dialog because it is no longer needed
+							 */
+							dialog.dismiss();
+
+							/*
+							 * Start the browser login with Facebook as login
+							 * service and the Facebook accesstoken
+							 */
+							openBrowserLogin("facebook",
+									session.getAccessToken());
+						}
+					});
+			builder.setNegativeButton(R.string.dialog_button_no,
+					new OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							/*
+							 * Close the dialog because it is no longer needed
+							 */
+							dialog.dismiss();
+						}
+					});
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		} else if (state.isClosed()) {
+			this.log.info("Logged out...");
+		}
+	}
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-		
+
 		EasyTracker.getInstance().activityStart(this);
 	}
-	
+
 	@Override
 	protected void onStop() {
 		super.onStop();
-		
+
 		EasyTracker.getInstance().activityStop(this);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		// For scenarios where the main activity is launched and user
+		// session is not null, the session state change notification
+		// may not be triggered. Trigger it if it's open/closed.
+		Session session = Session.getActiveSession();
+		if (session != null && (session.isOpened() || session.isClosed())) {
+			onSessionStateChange(session, session.getState(), null);
+		}
+
+		this.uiHelper.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		this.uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		this.uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		this.uiHelper.onSaveInstanceState(outState);
 	}
 
 	@Override
@@ -71,6 +174,15 @@ public class LoginActivity extends SherlockActivity {
 
 	public void openBrowserLogin(View view) {
 		Intent intent = new Intent(this, BrowserLoginActivity.class);
+		startActivityForResult(intent, LoginActivity.ACTIVITY_LOGIN);
+	}
+
+	public void openBrowserLogin(String loginProvider, String accesstoken) {
+		log.info("Openening browser login using provider and accesstoken");
+
+		Intent intent = new Intent(this, BrowserLoginActivity.class);
+		intent.putExtra(BrowserLoginActivity.EXTRA_PROVIDER, loginProvider);
+		intent.putExtra(BrowserLoginActivity.EXTRA_ACCESS_TOKEN, accesstoken);
 		startActivityForResult(intent, LoginActivity.ACTIVITY_LOGIN);
 	}
 
@@ -111,6 +223,8 @@ public class LoginActivity extends SherlockActivity {
 				break;
 			}
 		}
+
+		this.uiHelper.onActivityResult(requestCode, resultCode, data);
 	}
 
 	private class LoginTask extends SetupDeviceTask {
